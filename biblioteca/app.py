e01ee7df0261609fe50454d6b63a059fb92d8985
alunosNET <alunosnet@gmail.com>
2024-02-21 13:07:39 +0000

    Exercício biblioteca

--- biblioteca/app.py
+++ biblioteca/app.py
@@ -4,25 +4,81 @@ import numpy as np
 # O livro novo estará sempre disponivel
 # Verifique se ainda existe espaço no array antes de adicionar
 # Tenha atenção para não repetir o id do livro
-def adicionar_livro():
-    pass
+def adicionar_livro(livros,nr_livros):
+    if nr_livros==len(livros):
+        print("Não é possível adicionar mais livros.")
+        return
+    #ler os dados do livro
+    titulo=input("Título:")
+    disponivel=True
+    nome_leitor=""
+    id=livros[nr_livros-1]["id_livro"] + 1
+    livros[nr_livros]={"id_livro":id,"titulo":titulo,"disponivel":disponivel,"nome_leitor":nome_leitor}
+    nr_livros = nr_livros + 1
+    return nr_livros
 
 # Função para mostrar os livros
 # deve permitir escolher se lista todos ou só os disponíveis ou só os indisponíveis
-def listar_livros():
-    pass
+def listar_livros(livros,nr_livros):
+    op = input("1. Listar todos\n2. Listar disponíveis\n3. Listar indisponíveis: ")
+    if op=="1":
+        for i in range(nr_livros):
+            for chave, valor in livros[i].items():
+                print(f"{chave} - {valor}")
+            print("#"*20)
+    if op=="2" or op=="3":
+        escolha = True if op=="2" else False
+        for i in range(nr_livros):
+            if livros[i]['disponivel']==escolha:
+                for chave, valor in livros[i].items():
+                    print(f"{chave} - {valor}")
+                print("#"*20)
 
 # Função que muda o estado do livro e regista o nome do leitor
-def emprestar_livro():
-    pass
+def emprestar_livro(livros,nr_livros):
+    #pedir o id do livro
+    id = int(input("Id do livro a emprestar: "))
+    #verificar se o id existe e se o livro está disponível
+    for i in range(nr_livros):
+        if livros[i]["id_livro"]==id:
+            if livros[i]["disponivel"]==False:
+                print("Esse livro não está disponível")
+            else:
+                #pedir o nome do leitor
+                nome=input("Nome do leitor:")
+                #alterar o estado do livro
+                livros[i]["disponivel"]=False
+                #registar o nome do leitor
+                livros[i]["nome_leitor"]=nome
+            return
+    print("Não existe um livro com o id indicado")
 
 #Função que torna o livro disponível e apaga o nome do leitor
-def devolver_livro():
-    pass
+def devolver_livro(livros,nr_livros):
+    #pedir o id do livro
+    id = int(input("Id do livro a emprestar: "))
+    #verificar se o id existe e se o livro está disponível
+    for i in range(nr_livros):
+        if livros[i]["id_livro"]==id:
+            if livros[i]["disponivel"]==True:
+                print("Esse livro não está emprestado")
+            else:
+                #alterar o estado do livro
+                livros[i]["disponivel"]=not livros[i]['disponivel']
+                #remover o nome do leitor
+                livros[i]["nome_leitor"]=""
+            return
+    print("Não existe um livro com o id indicado")
 
 #Função que apresenta as opções ao utilizador e devolve a sua escolha
 def menu():
-    pass
+    while True:
+        print("1. Adicionar\n2.Listar\n3.Emprestar\n4.Devolver\n5.Sair")
+        op = int(input(":"))
+        if op <1 or op >5:
+            print("Essa opção não é válida")
+        else:
+            return op
 
 #função para gerar (seed) dados iniciais
 def inicializar(livros):
@@ -37,7 +93,18 @@ def main():
     MAX_ITEMS=100
     livros =np.empty(MAX_ITEMS,dtype=object)
     nr_livros=inicializar(livros)
-    #continuar aqui
+    op = 1
+    while op!=5:
+        op = menu()
+        if op==1:
+            nr_livros=adicionar_livro(livros,nr_livros)
+        elif op==2:
+            listar_livros(livros,nr_livros)
+        elif op==3:
+            emprestar_livro(livros,nr_livros)
+        elif op==4:
+            pass
+    print("Obrigado por utilizar o meu programa")
 
 # Ponto de entrada do programa (main loop)
 if __name__ == "__main__":
